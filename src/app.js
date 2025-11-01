e40f4f7844757530268c0b38861f87973bc26bba
Steven Acero <stevenacero07@gmail.com>
2025-11-01 02:15:18 +0000

    dominios final

--- src/app.js
+++ src/app.js
@@ -1,29 +1,14 @@
-// import "bootstrap";
-// import "./style.css";
+import "bootstrap";
+import "./style.css";
 
 
-// import "./assets/img/rigo-baby.jpg";
-// import "./assets/img/4geeks.ico";
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
 
-// window.onload = function() {
-//   //write your code here
-//   console.log("Hello Rigo from the console!");
-// };
-
-// let pronoun = ['the', 'our', 'my'];
-// let adj = ['great', 'big', 'best', 'smart'];
-// let noun = ['jogger', 'racoon', 'developer', 'coder'];
-// let extensions = ['.com', '.net', '.us', '.io', '.org', '.dev'];
-
-
-// pronombres.forEach(prom => {
-//   adjetivos.forEach(adj => {
-//     sustantivos.forEach(sus => {
-      
-//       console.log(prom, adj, sus)
-//     })
-//   })
-// });
+window.onload = function() {
+  //write your code here
+  console.log("Hello Rigo from the console!");
+};
 
 // Listas de palabras
 let pronoun = ['the', 'our', 'my'];
@@ -31,20 +16,42 @@ let adj = ['great', 'big', 'best', 'smart'];
 let noun = ['developer', 'coder', 'delicious', 'genius', 'previous'];
 let extensions = ['.com', '.net', '.us', '.io', '.org', '.dev'];
 
+  let dominiosNormales = [];
+  let dominiosHack = [];
+
 pronoun.forEach(pron => {
   adj.forEach(adj => {
     noun.forEach(noun => {
       let nombreDeDominios = pron+ adj+ noun ;
         extensions.forEach(ext => {          
           if(noun.endsWith(ext.slice(1))){
-            console.log(pron+ adj+noun.slice(0,-ext.slice(1).length)+ ext)
+            // console.log(pron+ adj+noun.slice(0,-ext.slice(1).length)+ ext)
+            dominiosHack.push(pron + adj + noun.slice(0, - ext.slice(1).length) + ext);
           } else{
-            let nombreDeDominiosConExt = nombreDeDominios + ext;
-          console.log(nombreDeDominiosConExt)
+            dominiosNormales.push(nombreDeDominios + ext);
+            // console.log(nombreDeDominiosConExt)
           }
           
 
         })
     })
   })
+});
+
+let tbodyNormales = document.getElementById('dominiosNormales');
+dominiosNormales.forEach((dominio, index) => {
+  let row = `<tr>
+    <td>${index + 1}</td>
+    <td>${dominio}</td>
+  </tr>`;
+  tbodyNormales.innerHTML += row;
+});
+
+let tbodyHack = document.getElementById('dominiosHack');
+dominiosHack.forEach((dominio, index) => {
+  let row = `<tr>
+    <td>${index + 1}</td>
+    <td>${dominio}</td>
+  </tr>`;
+  tbodyHack.innerHTML += row;
 });
