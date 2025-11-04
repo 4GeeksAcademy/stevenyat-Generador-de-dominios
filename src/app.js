4bad0fd2ee1b47baccb5b17bf031c49eded1aae6
Steven Acero <stevenacero07@gmail.com>
2025-11-04 03:35:23 +0000

    correction

--- src/app.js
+++ src/app.js
@@ -5,35 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = function () {
   console.log("Hello Rigo from the console!");
 };
 
-// Listas de palabras
 let pronoun = ['the', 'our', 'my'];
 let adj = ['great', 'big', 'best', 'smart'];
 let noun = ['developer', 'coder', 'delicious', 'genius', 'previous'];
 let extensions = ['.com', '.net', '.us', '.io', '.org', '.dev'];
 
-  let dominiosNormales = [];
-  let dominiosHack = [];
+let dominiosNormales = [];
+let dominiosHack = [];
 
 pronoun.forEach(pron => {
   adj.forEach(adj => {
     noun.forEach(noun => {
-      let nombreDeDominios = pron+ adj+ noun ;
-        extensions.forEach(ext => {          
-          if(noun.endsWith(ext.slice(1))){
-            // console.log(pron+ adj+noun.slice(0,-ext.slice(1).length)+ ext)
-            dominiosHack.push(pron + adj + noun.slice(0, - ext.slice(1).length) + ext);
-          } else{
-            dominiosNormales.push(nombreDeDominios + ext);
-            // console.log(nombreDeDominiosConExt)
-          }
-          
-
-        })
+      let nombreDeDominio = pron + adj + noun;
+      extensions.forEach(ext => {
+        if (noun.endsWith(ext.slice(1))) {
+          let nounHack = noun.slice(0, - ext.slice(1).length);
+          let dominioHack = pron + adj + nounHack;
+          dominiosHack.push(dominioHack + ext);
+        } else {
+          dominiosNormales.push(nombreDeDominio + ext);
+        }
+      })
     })
   })
 });
